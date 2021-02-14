1a534dc67c48a969511fb91aada538fedbeacd41
emelin1753 <emelin1753@gmail.com>
2021-02-14 14:26:46 +0300

    add count words

--- myfirstproject/views.py
+++ myfirstproject/views.py
@@ -10,5 +10,11 @@ def home(request):
 
 def reverse(request):
 	user_text = request.GET['usertext']
+	count_words = len(user_text.split())
 	reverse_text = user_text[::-1]
-	return render(request, 'reverse.html', {'usertext': user_text, 'reversetext': reverse_text})
+	return render(request, 'reverse.html', 
+		{
+		'usertext': user_text, 
+		'reversetext': reverse_text, 
+		'countwords': count_words
+		})
